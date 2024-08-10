0d6f11895bd36ef09e02a5d4616fc6e9178dd0c9
Nick Kennedy <115908031+nickkennedi@users.noreply.github.com>
2024-08-10 18:42:05 -0300

    Merge pull request #1
    * Diretorios add

--- app/src/main/java/com/example/kotlinkreatorsproject/View/SplashScreenActivity.kt
+++ app/src/main/java/com/example/kotlinkreatorsproject/View/SplashScreenActivity.kt
@@ -1,16 +1,15 @@
-package com.example.kotlinkreatorsproject
+package com.example.kotlinkreatorsproject.View
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.kotlinkreatorsproject.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
 
     }
 }
