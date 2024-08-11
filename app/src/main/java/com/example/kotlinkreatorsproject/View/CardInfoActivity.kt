9c030b4bc090946f143aaf14327a1ef8f3cf5407
nickkennedy <nickconta10@gmail.com>
2024-08-10 22:45:19 -0300

    Recycle view Add

--- app/src/main/java/com/example/kotlinkreatorsproject/View/CardInfoActivity.kt
+++ app/src/main/java/com/example/kotlinkreatorsproject/View/CardInfoActivity.kt
@@ -1,21 +1,17 @@
 package com.example.kotlinkreatorsproject.View
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.kotlinkreatorsproject.R
+import com.example.kotlinkreatorsproject.databinding.ActivityCardInfoBinding
 
 class CardInfoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCardInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_card_info)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityCardInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        
+        
     }
 }
